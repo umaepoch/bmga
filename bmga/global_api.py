8e7b315892c0cd7d75d575299395071c632dff66
karthikAv98 <karthikeyan.av98@gmail.com>
2022-05-27 09:08:40 +0530

    batchless update price fom purchase receipt

--- bmga/global_api.py
+++ bmga/global_api.py
@@ -33,12 +33,46 @@ def update_batch_price(item):
 
 		batch.save()
 
+def create_batchless_price(item):
+	p = frappe.db.sql(
+		f"""select name from `tabRate Contract` where selling_price = 1""", as_dict=1
+	)
+
+	if len(p) > 0:
+		selling = frappe.get_doc("Rate Contract", p[0]["name"])
+		selling.append("item", {
+			"item": item.get('item_code'),
+			"batched_item": "No",
+			"selling_price_for_customer": item.get('pch_mrp'),
+			"discount_percentage_for_customer_from_mrp": 0
+		})
+		
+		selling.save()
+
+def update_batchless_price(item):
+	rc = frappe.db.sql(
+		f"""select rci.name, rci.parent
+		from `tabRate Contract Item` as rci
+			join `tabRate Contract` as rc on (rci.parent = rc.name)
+		where rci.item = '{item.get('item_code')}' and rc.selling_price = 1""",
+		as_dict=1
+	)
+
+	if len(rc) > 0:
+		selling = frappe.get_doc("Rate Contract Item", rc[0]["name"])
+		selling.selling_price_for_customer = item.get('pch_mrp')
+
+		selling.save()
+	else :
+		create_batchless_price(item)
+
 # Purchase receipt
 @frappe.whitelist()
 def update_price_list_batch(items):
 	items = json.loads(items)
 	items = list(filter(lambda x: x.get('pch_fields') == 1, items))
 	for i in items:
-		if i.get('pch_mrp') == 0 or i.get('batch_no') == None: continue
-		update_batch_price(i)
+		if i.get('pch_mrp') == 0: continue
+		if i.get('batch_no') == "": update_batchless_price(i)
+		else: update_batch_price(i)
 	return dict(items = items)
