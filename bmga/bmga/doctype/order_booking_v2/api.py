4aadb4e497dc79d7578d0e0f89b61acbd64994b5
karthikAv98 <karthikeyan.av98@gmail.com>
2022-09-27 16:32:38 +0530

    print outstanding amount

--- bmga/bmga/doctype/order_booking_v2/api.py
+++ bmga/bmga/doctype/order_booking_v2/api.py
@@ -980,41 +980,42 @@ def get_credit_limit(customer, company):
 	return flt(credit_limit)
 
 def get_customer_outstanding(customer, company, ignore_outstanding_sales_order=False, cost_center=None):
-	# Outstanding based on GL Entries
+    # Outstanding based on GL Entries
 
-	cond = ""
-	if cost_center:
-		lft, rgt = frappe.get_cached_value("Cost Center",
-			cost_center, ['lft', 'rgt'])
+    cond = ""
+    if cost_center:
+        lft, rgt = frappe.get_cached_value("Cost Center",
+            cost_center, ['lft', 'rgt'])
+        
+        cond = """ and cost_center in (select name from `tabCost Center` where
+            lft >= {0} and rgt <= {1})""".format(lft, rgt)
+    
+    outstanding_based_on_gle = frappe.db.sql("""
+        select sum(debit) - sum(credit)
+        from `tabGL Entry` where party_type = 'Customer'
+        and party = %s and company=%s {0}""".format(cond), (customer, company))
+    
+    outstanding_based_on_gle = flt(outstanding_based_on_gle[0][0]) if outstanding_based_on_gle else 0
 
-		cond = """ and cost_center in (select name from `tabCost Center` where
-			lft >= {0} and rgt <= {1})""".format(lft, rgt)
+    # Outstanding based on Sales Order
+    outstanding_based_on_so = 0
 
-	outstanding_based_on_gle = frappe.db.sql("""
-		select sum(debit) - sum(credit)
-		from `tabGL Entry` where party_type = 'Customer'
-		and party = %s and company=%s {0}""".format(cond), (customer, company))
+    # if credit limit check is bypassed at sales order level,
+    # we should not consider outstanding Sales Orders, when customer credit balance report is run
 
-	outstanding_based_on_gle = flt(outstanding_based_on_gle[0][0]) if outstanding_based_on_gle else 0
+    if not ignore_outstanding_sales_order:
+        outstanding_based_on_so = frappe.db.sql("""
+            select sum(base_grand_total*(100 - per_billed)/100)
+            from `tabSales Order`
+            where customer=%s and docstatus = 1 and company=%s
+            and per_billed < 100 and status != 'Closed'""", (customer, company))
+        
+        outstanding_based_on_so = flt(outstanding_based_on_so[0][0]) if outstanding_based_on_so else 0
+    
+    # Outstanding based on Delivery Note, which are not created against Sales Order
+    outstanding_based_on_dn = 0
 
-	# Outstanding based on Sales Order
-	outstanding_based_on_so = 0
-
-	# if credit limit check is bypassed at sales order level,
-	# we should not consider outstanding Sales Orders, when customer credit balance report is run
-	if not ignore_outstanding_sales_order:
-		outstanding_based_on_so = frappe.db.sql("""
-			select sum(base_grand_total*(100 - per_billed)/100)
-			from `tabSales Order`
-			where customer=%s and docstatus = 1 and company=%s
-			and per_billed < 100 and status != 'Closed'""", (customer, company))
-
-		outstanding_based_on_so = flt(outstanding_based_on_so[0][0]) if outstanding_based_on_so else 0
-
-	# Outstanding based on Delivery Note, which are not created against Sales Order
-	outstanding_based_on_dn = 0
-
-	unmarked_delivery_note_items = frappe.db.sql("""select
+    unmarked_delivery_note_items = frappe.db.sql("""select
 			dn_item.name, dn_item.amount, dn.base_net_total, dn.base_grand_total
 		from `tabDelivery Note` dn, `tabDelivery Note Item` dn_item
 		where
@@ -1024,11 +1025,11 @@ def get_customer_outstanding(customer, company, ignore_outstanding_sales_order=F
 			and ifnull(dn_item.against_sales_order, '') = ''
 			and ifnull(dn_item.against_sales_invoice, '') = ''
 		""", (customer, company), as_dict=True)
-
-	if not unmarked_delivery_note_items:
-		return outstanding_based_on_gle + outstanding_based_on_so
-
-	si_amounts = frappe.db.sql("""
+    
+    if not unmarked_delivery_note_items:
+        return outstanding_based_on_gle + outstanding_based_on_so
+    
+    si_amounts = frappe.db.sql("""
 		SELECT
 			dn_detail, sum(amount) from `tabSales Invoice Item`
 		WHERE
@@ -1040,17 +1041,92 @@ def get_customer_outstanding(customer, company, ignore_outstanding_sales_order=F
 		))
 	)
 
-	si_amounts = {si_item[0]: si_item[1] for si_item in si_amounts}
+    si_amounts = {si_item[0]: si_item[1] for si_item in si_amounts}
 
-	for dn_item in unmarked_delivery_note_items:
-		dn_amount = flt(dn_item.amount)
-		si_amount = flt(si_amounts.get(dn_item.name))
+    for dn_item in unmarked_delivery_note_items:
+        dn_amount = flt(dn_item.amount)
+        si_amount = flt(si_amounts.get(dn_item.name))
 
-		if dn_amount > si_amount and dn_item.base_net_total:
-			outstanding_based_on_dn += ((dn_amount - si_amount)
-				/ dn_item.base_net_total) * dn_item.base_grand_total
+        if dn_amount > si_amount and dn_item.base_net_total:
+            outstanding_based_on_dn += ((dn_amount - si_amount)
+                / dn_item.base_net_total) * dn_item.base_grand_total
+    
+    print(outstanding_based_on_gle, outstanding_based_on_so, outstanding_based_on_dn)
 
-	return outstanding_based_on_gle + outstanding_based_on_so + outstanding_based_on_dn
+    return outstanding_based_on_gle + outstanding_based_on_so + outstanding_based_on_dn
+
+# def get_customer_outstanding(customer, company, ignore_outstanding_sales_order=False, cost_center=None):
+# 	# Outstanding based on GL Entries
+
+# 	cond = ""
+# 	if cost_center:
+# 		lft, rgt = frappe.get_cached_value("Cost Center",
+# 			cost_center, ['lft', 'rgt'])
+
+# 		cond = """ and cost_center in (select name from `tabCost Center` where
+# 			lft >= {0} and rgt <= {1})""".format(lft, rgt)
+
+# 	outstanding_based_on_gle = frappe.db.sql("""
+# 		select sum(debit) - sum(credit)
+# 		from `tabGL Entry` where party_type = 'Customer'
+# 		and party = %s and company=%s {0}""".format(cond), (customer, company))
+
+# 	outstanding_based_on_gle = flt(outstanding_based_on_gle[0][0]) if outstanding_based_on_gle else 0
+
+# 	# Outstanding based on Sales Order
+# 	outstanding_based_on_so = 0
+
+# 	# if credit limit check is bypassed at sales order level,
+# 	# we should not consider outstanding Sales Orders, when customer credit balance report is run
+# 	if not ignore_outstanding_sales_order:
+# 		outstanding_based_on_so = frappe.db.sql("""
+# 			select sum(base_grand_total*(100 - per_billed)/100)
+# 			from `tabSales Order`
+# 			where customer=%s and docstatus = 1 and company=%s
+# 			and per_billed < 100 and status != 'Closed'""", (customer, company))
+
+# 		outstanding_based_on_so = flt(outstanding_based_on_so[0][0]) if outstanding_based_on_so else 0
+
+# 	# Outstanding based on Delivery Note, which are not created against Sales Order
+# 	outstanding_based_on_dn = 0
+
+# 	unmarked_delivery_note_items = frappe.db.sql("""select
+# 			dn_item.name, dn_item.amount, dn.base_net_total, dn.base_grand_total
+# 		from `tabDelivery Note` dn, `tabDelivery Note Item` dn_item
+# 		where
+# 			dn.name = dn_item.parent
+# 			and dn.customer=%s and dn.company=%s
+# 			and dn.docstatus = 1 and dn.status not in ('Closed', 'Stopped')
+# 			and ifnull(dn_item.against_sales_order, '') = ''
+# 			and ifnull(dn_item.against_sales_invoice, '') = ''
+# 		""", (customer, company), as_dict=True)
+
+# 	if not unmarked_delivery_note_items:
+# 		return outstanding_based_on_gle + outstanding_based_on_so
+
+# 	si_amounts = frappe.db.sql("""
+# 		SELECT
+# 			dn_detail, sum(amount) from `tabSales Invoice Item`
+# 		WHERE
+# 			docstatus = 1
+# 			and dn_detail in ({})
+# 		GROUP BY dn_detail""".format(", ".join(
+# 			frappe.db.escape(dn_item.name)
+# 			for dn_item in unmarked_delivery_note_items
+# 		))
+# 	)
+
+# 	si_amounts = {si_item[0]: si_item[1] for si_item in si_amounts}
+
+# 	for dn_item in unmarked_delivery_note_items:
+# 		dn_amount = flt(dn_item.amount)
+# 		si_amount = flt(si_amounts.get(dn_item.name))
+
+# 		if dn_amount > si_amount and dn_item.base_net_total:
+# 			outstanding_based_on_dn += ((dn_amount - si_amount)
+# 				/ dn_item.base_net_total) * dn_item.base_grand_total
+
+# 	return outstanding_based_on_gle + outstanding_based_on_so + outstanding_based_on_dn
 
 
 def check_credit_limit(customer, company, ignore_outstanding_sales_order=False, extra_amount=0):
